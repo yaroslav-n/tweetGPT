a6609091946c9912fe04271a37fda5aac782011c
Yaroslav Nazarov <ya.nazarov@gmail.com>
2023-03-11 23:53:18 +1300

    fix: better text input (wip)

--- src/inject/dom/set_input_text.ts
+++ src/inject/dom/set_input_text.ts
@@ -1,8 +1,9 @@
 import { wait } from "../../utils/wait";
 
 export const setInputText = async (inputEl: HTMLInputElement, text: string) => {
-    inputEl.focus();
-    await wait(1);
-    try { document.execCommand('selectAll'); } catch(e) {}
-    try { document.execCommand('insertText', false, text); } catch(e) {}
+    const textWrapper = inputEl.querySelector('[data-text="true"]')?.parentElement;
+    if (textWrapper) {
+        textWrapper.innerHTML = `<span data-text="true">${text}</span>`;
+        textWrapper.dispatchEvent(new Event('input', { 'bubbles': true, 'cancelable': true }));
+    }
 };
