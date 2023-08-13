6325d28d1d977688ec243651dea47a498a8e57eb
jeff <jeff@shippie.ca>
2023-08-13 00:51:08 -0400

    fix set input text by removing placeholder text from input

--- src/inject/dom/set_input_text.ts
+++ src/inject/dom/set_input_text.ts
@@ -2,6 +2,11 @@ import { wait } from "../../utils/wait";
 
 export const setInputText = async (inputEl: HTMLInputElement, text: string) => {
     const textWrapper = inputEl.querySelector('[data-text="true"]')?.parentElement;
+    const elements = document.getElementsByClassName("public-DraftEditorPlaceholder-root");
+    const parentNode = elements[0].parentNode;
+    if (parentNode) {
+      parentNode.removeChild(elements[0]);
+    }
     if (textWrapper) {
         textWrapper.innerHTML = `<span data-text="true">${text}</span>`;
         textWrapper.dispatchEvent(new Event('input', { 'bubbles': true, 'cancelable': true }));
