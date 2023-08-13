d8962ef60c7135f263582895429fee7f8dacd579
jeff <jeff@shippie.ca>
2023-08-13 01:07:31 -0400

    fix set input text by removing all text from input

--- src/inject/dom/set_input_text.ts
+++ src/inject/dom/set_input_text.ts
@@ -3,9 +3,11 @@ import { wait } from "../../utils/wait";
 export const setInputText = async (inputEl: HTMLInputElement, text: string) => {
     const textWrapper = inputEl.querySelector('[data-text="true"]')?.parentElement;
     const elements = document.getElementsByClassName("public-DraftEditorPlaceholder-root");
-    const parentNode = elements[0].parentNode;
-    if (parentNode) {
-      parentNode.removeChild(elements[0]);
+    for (const element of elements) {
+      const parentNode = element.parentNode;
+      if (parentNode) {
+        parentNode.removeChild(element);
+      }
     }
     if (textWrapper) {
         textWrapper.innerHTML = `<span data-text="true">${text}</span>`;
